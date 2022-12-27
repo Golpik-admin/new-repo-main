d25499c14cc6c9a89b29fe477ce09d06c8a89000
myameen <m.yameen@golpik.com>
2022-12-27 19:28:17 +0500

    material table

--- src/pages/userapp/alerts.js
+++ src/pages/userapp/alerts.js
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import React, { useEffect } from "react";
+import React, { forwardRef, useEffect } from "react";
 import styled from "@emotion/styled";
 import { Helmet } from "react-helmet-async";
 import { useDispatch, useSelector } from "react-redux";
@@ -22,7 +22,7 @@ import {
   Radio,
   TextField,
 } from "@mui/material";
-import { SyncAlt } from "@mui/icons-material";
+import { AddBox, ArrowDownward, Clear, DeleteOutline, FilterList, FirstPage, LastPage, Remove, SaveAlt, SyncAlt, ViewColumn } from "@mui/icons-material";
 import Stats from "./Stats";
 import { spacing } from "@mui/system";
 import { fetchAlerts } from "../../redux/slices/alerts";
@@ -39,6 +39,10 @@ import moment from "moment-timezone";
 import FilterPop from "./Filter";
 import useAuth from "../../hooks/useAuth";
 import { fetchSettings } from "../../redux/slices/getSettings";
+
+import MaterialTable from 'material-table';
+import { Check, ChevronLeft, ChevronRight, Edit, Search } from "react-feather";
+
 
 /* Declearation */
 const Divider = styled(MuiDivider)(spacing);
@@ -349,9 +353,50 @@ function EnhancedTable() {
   const LinearProgress = styled(MuiLinearProgress)(spacing);
 
 
+  const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+  };
 
   return (
     <div>
+      <MaterialTable
+      icons={tableIcons}
+title="Basic Filtering Preview"
+columns={[
+  { title: 'Name', field: 'name' },
+  { title: 'Surname', field: 'surname' },
+  { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+  {
+    title: 'Birth Place',
+    field: 'birthCity',
+    lookup: { 34: 'İstanbul', 63: 'Şanlıurfa',65:'Pakistan' },
+  },
+]}
+data={[
+  { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+  { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
+]}        
+options={{
+  filtering: true,
+  search:false
+}}
+/>
       {alertList.loading && <LinearProgress />}
       <Paper
         sx={{
